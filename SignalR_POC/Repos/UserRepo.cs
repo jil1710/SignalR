d7349f8be647d61ebeaff35ea92b385f9bf7861d
jil <jil.p@simformsolutions.com>
2023-11-06 16:28:18 +0530

    add: poc on signalR

---
+++ SignalR_POC/Repos/UserRepo.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SignalR_POC.Data;
+using SignalR_POC.Models;
+
+namespace SignalR_POC.Repos
+{
+    public class UserRepo
+    {
+        private readonly ApplicationDBContext dbContext;   
+
+        public UserRepo(ApplicationDBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<TblUser> GetUserDetails(string username, string password)
+        {
+            return await dbContext.TblUser.FirstOrDefaultAsync(user => user.Username == username && user.Password == password);
+        }
+    }
+}
---
+++ SignalR_POC/SubscribeTableDependencies/ISubscribeTableDependency.cs
@@ -0,0 +1,7 @@
+﻿namespace SignalR_POC.SubscribeTableDependencies
+{
+    public interface ISubscribeTableDependency
+    {
+        void SubscribeTableDependency(string connectionString);
+    }
+}
